0e34583a6a0d5d7d6bbf0483c32c5f2598ab2251
Su Leng <buixuanlong18@gmail.com>
2023-02-15 22:16:46 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { CardMedia, Grid, Item } from '@mui/material';
+import React from 'react';
+import { dataStories } from './data/DataFake';
+import { flexbox } from '@mui/system';
+import { WidthFull } from '@mui/icons-material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box className="App">
+      <Box mt={5}>
+        <Typography variant="h1" gutterBottom align="center">
+          Stories
+        </Typography>
+      </Box>
+      <Box sx={{ width: 700, overflow: 'hidden', margin: '0 auto' }}>
+        <Box
+          sx={{
+            width: 900,
+            display: 'flex',
+            justifyContent: 'center',
+            gap: '15px',
+            overflowX: 'hidden',
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {dataStories.length > 0 &&
+            dataStories.map((item) => (
+              <Box
+                sx={{ width: 120, height: 200, borderRadius: '20px', position: 'relative' }}
+                key={item.name}
+              >
+                <Box
+                  sx={{
+                    width: '25px',
+                    height: '25px',
+                    position: 'absolute',
+                    zIndex: '10',
+                    top: '20px',
+                    left: '20px',
+                    backgroundColor: '#5260d4',
+                    padding: '4px',
+                    borderRadius: '100px',
+                  }}
+                >
+                  <img
+                    src={item.avatar}
+                    alt=""
+                    style={{
+                      width: '100%',
+                      height: '100%',
+                      borderRadius: '100px',
+                      display: 'inline-block',
+                    }}
+                  />
+                </Box>
+                <img
+                  style={{
+                    width: '100%',
+                    height: '100%',
+                    objectFit: 'cover',
+                    borderRadius: '10px',
+                  }}
+                  src={item.image}
+                  alt={item.time}
+                />
+              </Box>
+            ))}
+        </Box>
+      </Box>
+    </Box>
   );
 }
 
---
+++ src/data/DataFake.js
@@ -0,0 +1,50 @@
+export const dataStories = [
+  {
+    name: 'Su Doe',
+    avatar: 'https://source.unsplash.com/random/430x',
+    time: '2 hours ago',
+    image: 'https://source.unsplash.com/random/430x',
+  },
+  {
+    name: 'Jane Home',
+    avatar: 'https://source.unsplash.com/random/32x32',
+    time: '3 hours ago',
+    image: 'https://source.unsplash.com/random/470x',
+  },
+  {
+    name: 'Man City',
+    avatar: 'https://source.unsplash.com/random/32x32',
+    time: '1 hours ago',
+    image: 'https://source.unsplash.com/random/480x',
+  },
+  {
+    name: 'Chu Tu',
+    avatar: 'https://source.unsplash.com/random/32x32',
+    time: '23 hours ago',
+    image: 'https://source.unsplash.com/random/400x',
+  },
+  {
+    name: 'Real',
+    avatar: 'https://source.unsplash.com/random/32x32',
+    time: '10 hours ago',
+    image: 'https://source.unsplash.com/random/401x',
+  },
+  {
+    name: 'Barca3',
+    avatar: 'https://source.unsplash.com/random/32x32',
+    time: '19 hours ago',
+    image: 'https://source.unsplash.com/random/420x',
+  },
+  {
+    name: 'Barca1',
+    avatar: 'https://source.unsplash.com/random/32x32',
+    time: '19 hours ago',
+    image: 'https://source.unsplash.com/random/440x',
+  },
+  {
+    name: 'Barca2',
+    avatar: 'https://source.unsplash.com/random/32x32',
+    time: '19 hours ago',
+    image: 'https://source.unsplash.com/random/449x',
+  },
+];
